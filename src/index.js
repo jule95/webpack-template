d4f611e8d69915c25e52d76aa3266114d5adb4bd
Julius Plass <julius.plass@hotmail.com>
2020-07-07 14:53:55 +0200

    changed project structure to react project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//react
+import React from "react";
+import ReactDOM from "react-dom";
+
+//app component
+import App from "./containers/App";
+
+ReactDOM.render(<App title="I Am A React App!"/>, document.getElementById('root'));
