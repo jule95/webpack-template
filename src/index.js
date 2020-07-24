d8c102a19f40201b344b028f4873256a5661a666
Julius Plass <julius.plass@hotmail.com>
2020-07-24 14:44:34 +0200

    added react bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 //react
 import React from "react";
 import ReactDOM from "react-dom";
+//react bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //app component
 import App from "./containers/App";
