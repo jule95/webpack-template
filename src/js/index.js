8df30520eaa11ac157be8b2e80f91a9086329244
Julius Plass <julius.plass@hotmail.com>
2020-09-27 13:35:01 +0200

    added bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+//Bootstrap
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//Vue
 import Vue from 'vue';
 import App from '../App.vue';
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
